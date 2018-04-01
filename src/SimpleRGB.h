3a8d6eeb5c230574d1c3931a919450cb7f529791
Jorge Villalobos <jorgemvc@gmail.com>
2018-04-01 09:52:53 -0600

    Update SimpleRGB.h

--- src/SimpleRGB.h
+++ src/SimpleRGB.h
@@ -20,11 +20,11 @@ class SensorRGB {
      // ---- Methods
      bool     begin();
      void     getRGBColor();
-     uint16_t Red();
-     uint16_t Green();
-     uint16_t Blue();
-     uint16_t Clear();
-     float    Diff(uint16_t r, uint16_t g, uint16_t b);
+     uint16_t red();
+     uint16_t green();
+     uint16_t blue();
+     uint16_t clear();
+     float    diff(uint16_t r, uint16_t g, uint16_t b);
      
    private:
      uint16_t  _r,
